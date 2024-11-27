ccbfefb0e969cb2f48b858a24ca97b901bd769b5
Sr Asafe <anteroasafe07@gmail.com>
2024-11-26 22:54:30 -0400

    fim aula 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -16,10 +16,54 @@ purchase_orders = [
     }
 ]
 
+#GET purchase_orders
+#GET purchase_orders_by_id
+#POST purchase_orders
+#GET purchase_orders_items
+#POST purchase_orders_items
 
 @app.route('/')
 def home():
-    return "Hello World! alterado"
+    return "Hello World!!"
+
+@app.route('/purchase_orders')
+def get_purchade_orders():
+    return jsonify(purchase_orders)
+
+@app.route('/purchase_orders/<int:id>/items')
+def get_purchase_orders_items(id):    
+    for po in purchase_orders:
+        if po ['id'] == id:
+            return jsonify (po['items'])
+
+    return jsonify({'message': 'Pedido {} Não Encontrado'. format(id)})
+
+@app.route('/purchase_orders', methods=['POST'])
+def create_purchase_order():
+    request_data = request.get_json()
+    purchase_order =  {                                                                                                                                                         
+           'id':request_data['id'],
+           'description': request_data['description'],
+           'items': []
+    }
+
+    purchase_orders.append(purchase_order)
+     
+    return jsonify(purchase_order)
+
+@app.route('/purchase_orders/<int:id>/items', methods=['POST'])
+def create_purchase_orders_items(id):
+    req_data = request.get_json()
+    for po in purchase_orders:
+        if po['id'] == id:
+            po['items'].append({
+                    'id': req_data['id'],
+                    'description': req_data['description'],
+                    'price': req_data['price']
+               })
+            return jsonify(po)
+
+    return jsonify({'message': 'Pedido {} Não Encontrado'. format(id)})
 
 
-app.run(port=3000)
+app.run(port=5000)
