38651717e167adbf321b93343f5d567700b7ba72
Antero Asafe <anteroasafe07@gmail.com>
2024-12-23 18:54:38 -0400

    retaforando purchase_order_item para implementar post

---
+++ purchase_orders_items/resources.py
@@ -0,0 +1,19 @@
+from flask import jsonify
+from flask_restful import Resource
+
+purchase_orders = [
+    {
+        "id": 1,
+        "description": "Purchase Order id 1",
+        "items": [{"id": 1, "description": "Item do pedido 1", "price": 19.99}],
+    }
+]
+
+
+class PurchaseOrdersItems(Resource):
+
+    def get(self, id):
+        for po in purchase_orders:
+            if po["id"] == id:
+                return jsonify(po["items"])
+            return jsonify({"message": "pedido de id{} n encontrado".format(id)})
---
+++ tests/purchase_orders_items/test_resources.py
@@ -0,0 +1,39 @@
+import json
+
+def test_get_items_by_purchase_order_id(test_client):
+    response = test_client.get('/purchase_orders/1/items')
+
+    assert response.status_code == 200
+    assert len(response.json) == 1
+    assert response.json[0]['id'] == 1
+    assert response.json[0]['description'] == 'item do pedido 1'
+    assert response.json[0]['price'] == 17.99
+
+def test_get_items_by_purchase_order_id_not_found(test_client):
+    id = 9999
+    response = test_client.get('/purchase_orders/{}/items'.format(id))
+    
+    assert response.status_code == 200
+    assert response.json['message'] == 'Pedido de id {} n encontrado'.format(
+        id)
+    
+
+def test_post_purchase_order_item(test_client):
+    obj = {
+        'id': 2,
+        'description': 'Item teste',
+        'price': 10.0
+    }
+
+    response = test_client.post(
+        '/purchase_order_items/1/items',
+        data=json.dumps(obj),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 200
+    assert response.json['id'] == 1
+    assert len (response.json['items']) == 2
+    assert response.json['items'][1]['id'] == obj['id']
+    assert response.json['items'][1]['description'] == obj['description']
+    assert response.json['items'][1]['price'] == obj['price']
